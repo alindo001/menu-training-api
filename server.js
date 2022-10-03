ab0dc091db8d917d2a285e702a64b345e5fe3eb9
Andres <73503197+alindo001@users.noreply.github.com>
2022-10-03 10:50:19 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,34 +9,33 @@ app.use(cors())
 
 
 
-const menuItems = [{ 
-    "char_id":1,
-    'porkBowl': {
-    'itemName' : 'Pork Belly Noodle Bowl',
-    'ingredients': 'Noodle, Pork Belly, Broth',
-    'price':'$24.00',
-    'img': 'https://media-cdn.tripadvisor.com/media/photo-m/1280/18/cd/03/93/pork-belly-noodle-bowl.jpg' ,
-    
-    
-},   
-    "char_id":2,    
-    'airlineChick': {
-    'itemName' : 'Asian-marinated Airline Chicken',
-    'ingredients': 'Sticky Rice, Bok Choy, Asian Glaze',
-    'price':'$24',
-    'img': 'https://media.wdwnt.com/2019/10/new-kona-cafe-menu-sticky-wings-airplane-chicken-polynesian-resort_17-1200x675.jpg' ,
-    
-
-},   "char_id":3,
-    'turkey': {
-    'itemName' : 'Turkey Banh Mi',
-    'ingredients': 'Slow-roasted Turkey Breast, Black Pepper Bacon, Pork Pâte, Cilantro, Jalapeño, Onions, and Grilled Poblano Mayonnaise served with French Fries',
-    'price':'$18',
-    'img': 'https://i1.wp.com/www.wdwopinion.com/wp-content/uploads/2020/01/IMG_8386-scaled.jpg?fit=1024%2C768&ssl=1' ,
-    
+const menuItems = [ {
+    "char_id": 1,
+    "itemName": "Pork Belly Noodle Bowl",
+    "price": "$24.00",
+    "img": "https://media-cdn.tripadvisor.com/media/photo-m/1280/18/cd/03/93/pork-belly-noodle-bowl.jpg",
+    "ingredients": "Noodles, Pork Belly, Broth",
+    "allergens": "gluten, eggs, soy",
+},
+{
+    "char_id": 2,
+    "itemName": "Asian-marinated Airline Chicken",
+    "price": "$24.00",
+    "img": "https://media.wdwnt.com/2019/10/new-kona-cafe-menu-sticky-wings-airplane-chicken-polynesian-resort_17-1200x675.jpg",
+    "ingredients": "Sticky Rice, Bok Choy, Asian Glaze",
+    "allergens": "dairy, soy",
+},
+{
+    "char_id": 3,
+    "itemName": "Turkey Banh Mi",
+    "price": "$18.00",
+    "img": "https://i1.wp.com/www.wdwopinion.com/wp-content/uploads/2020/01/IMG_8386-scaled.jpg?fit=1024%2C768&ssl=1",
+    "ingredients": "Slow-roasted Turkey Breast, Black Pepper Bacon, Pork Pâte, Cilantro, Jalapeño, Onions, and Grilled Poblano Mayonnaise served with French Fries",
+    "allergens": "gluten, dairy",
 },
 
-}]
+
+]
 
 app.get('/api/menuItems', (req,res) => {
     res.json(menuItems)
